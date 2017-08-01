33ae90d68e0449262d9a7014a76782db3fdcedc9
xixiaohai <xiaohai.xi@jyblife.com>
2017-08-01 14:42:21 +0800

    feat:添加组件tips

---
+++ packages/lib-tips/src/index.js
@@ -0,0 +1,81 @@
+class Tips {
+  constructor() {
+    this.tipsHtml = null;
+    this.ptr      = null;
+    this.config = {
+      msg: '',
+      isLoading: false,
+      autoHide: true,
+      hideTime: 1200
+    };
+
+    this.iconConf = {
+      loading: '<span class="rotate-icon"></span>',
+      none: ''
+    };
+
+    this.tipTemplate = '<div id="bubble"><div class="mod-spinner">' +
+      '<div class="spinner-wrap">{{icon}}' +
+      '<p class="text" id="bubble-text">{{text}}</p>' + 
+      '</div></div></div>';
+  }
+
+  showTips(option) {
+    if (this.tipsHtml) {
+      return 
+    }
+
+    const conf = Object.assign({}, this.config, option);
+    const html = this.tipTemplate
+      .replace(/{{icon}}/, conf.isLoading ? this.iconConf.loading: '')
+      .replace(/{{text}}/, conf.msg);
+
+    const container = document.createElement('div');
+    container.innerHTML = '' + html;
+    this.tipsHtml = container.children[0];
+    document.body.insertBefore(this.tipsHtml, null);
+
+    if (conf.autoHide) {
+      setTimeout(() => {
+        this.closeTips();
+      }, conf.hideTime);
+    }
+
+    this.lock = true;
+  }
+
+  showLoading(message) {
+    this.showTips({
+      msg: message || "努力加载中...",
+      isLoading: true,
+      autoHide: false
+    });
+  }
+
+  showError(message) {
+    clearInterval(this.ptr);
+    this.ptr = setInterval(() => {
+      if(!this.tipsHtml) {
+        this.showTips({
+            msg: message
+        });
+
+        clearInterval(this.ptr);
+        this.ptr = null;
+      }
+    }, 200);
+  }
+
+  closeTips() {
+    if(!this.tipsHtml) {
+        return this;
+    }
+
+    this.tipsHtml.parentNode.removeChild(this.tipsHtml);
+    this.tipsHtml = null;
+    this.lock = false;
+    return this;
+  }
+}
+
+export default new Tips
