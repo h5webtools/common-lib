a2b4ecb3748cc4289e70e1535222cbf89c0e832c
canye <canye.wu@jyblife.com>
2018-05-24 10:29:46 +0800

    chore: 修改文档构建配置

--- jfet.config.js
+++ jfet.config.js
@@ -8,6 +8,8 @@ module.exports = {
   doc() {
     return {
       name: pkg.name,
+      title: '前端公共库',
+      desc: '公共库文档，包含每个模块的使用，API，例子等等。',
       token: '21232F297A57A5A743894A0E4A801FC3',
       uploadUrl: 'http://doc.fe.jyb.com/api/upload'
     };
