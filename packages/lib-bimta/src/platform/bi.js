7c2c0a721a651539e25d7c8528966e674f0fc589
canye <canye.wu@jyblife.com>
2018-12-13 17:00:09 +0800

    feat(lib-bimta): url有mta_id参数app内也上报bi，添加from,referrer字段 #000

--- packages/lib-bimta/src/platform/bi.js
+++ packages/lib-bimta/src/platform/bi.js
@@ -74,9 +74,11 @@ class BI {
           op_page: '', // h5为空（可选）
           op_params: _.assign({
             platform: platformStr, // 平台
+            from: 'h5',
             in_app: os.jyb ? 1 : 0, // 1为加油宝app内，0为app外
             cust_id: custId, // 客户id
             uniq_id: custId || uuid, // 如果未登录设置此id,如果登录与custId一致
+            referrer: document.referrer,
             source: _.getQuery('channel') || _.getQuery('from') || '', // baidu
             act_id: '', // 活动ID
             group: '' // 用户群
